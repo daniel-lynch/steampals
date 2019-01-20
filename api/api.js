988fbc2dedf0255411b49e1da685cd77af56701b
= <=>
2019-01-20 18:47:18 -0500

    major code refactoring and clean up. Basic readme aswell

--- api/api.js
+++ api/api.js
@@ -1,53 +1,28 @@
 // Request library for api requests
 const request = require("request");
-// Apikey from file config.js see config.js.example
-const apikey = require("./config.js");
+const apikey = require("../config/config").steamApiKey;
 // my steam id for testing
 const mysteamid = "76561198041498404";
 
 // Get games function, pass in steamid of the user, appinfo=1 returns the game name
-exports.getGames = (steamid, callback) => {
-  request(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apikey}&steamid=${steamid}&format=json&include_appinfo=1`,
-    function (error, response, body) {
-      if (error) {
-        console.log(error);
-        callback(error, null);
-      } else {
-        gamesList = JSON.parse(body);
-        games = [];
-        //console.log(gamesList.response);
-        for (game in gamesList.response.games) {
-          games.push(gamesList.response.games[game].name);
-          //console.log(gamesList.response.games[game].name);
-        }
-        callback(null, games);
-      }
-    }
-  );
-};
-
-exports.getGamesarr = new Promise((resolve, reject) => {
-  (steamid) => {
-    request(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apikey}&steamid=${steamid}&format=json&include_appinfo=1`,
+exports.getGames = (steamId) => {
+  return new Promise((resolve, reject) => {
+    request(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apikey}&steamid=${steamId}&format=json&include_appinfo=1`,
       function (error, response, body) {
         if (error) {
           console.log(error);
           reject(error)
         } else {
           gamesList = JSON.parse(body);
-          console.log(gamesList)
           games = [];
-          //console.log(gamesList.response);
           for (game in gamesList.response.games) {
             games.push(gamesList.response.games[game].name);
-            //console.log(gamesList.response.games[game].name);
-            //callback(null, games);
           }
           resolve(games)
         }
       });
-  }
-});
+  });
+}
 
 // Get friends function, returns all friends steam id's, relations, and how long they've been friends
 exports.getFriends = (steamid, callback) => {
@@ -75,15 +50,11 @@ exports.getRealName = (steamids, callback) => {
         console.log(error);
       } else {
         res = JSON.parse(body);
-        //console.log(res)
         friends = []
         for (player in res.response.players) {
-          console.log(res.response.players[player]);
           friends.push(res.response.players[player])
         }
         callback(friends);
       }
     });
 }
-
-//getFriends(mysteamid);
---
+++ routes/index.js
@@ -0,0 +1,61 @@
+module.exports = (app, api) => {
+    app.get('/', function (req, res) {
+        if (req.user) {
+            api.getFriends(req.user.steamId, (friends) => {
+                api.getRealName(req.user.steamId, (user) => {
+                    res.render('index', { user, friends });
+                });
+            });
+        } else {
+            res.redirect('/auth/login')
+        }
+    });
+
+    app.get('/yikes', function (req, res) {
+        if (req.user) {
+            api.getFriends(req.user.steamId, (friends) => {
+                api.getRealName(req.user.steamId, (user) => {
+                    res.render('yikes', { user, friends });
+                });
+            });
+        } else {
+            res.redirect('/auth/login')
+        }
+    });
+
+
+    app.post('/yikes', async function (req, res) {
+        if (req.user) {
+            if (req.body.friends) {
+                const friends = req.body.friends
+                const totalUsers = friends.length + 1
+                let sameGames = [];
+                let compGames = [];
+                let counts = {};
+                let userGames = await api.getGames(req.user.steamId);
+                userGames = [...new Set(userGames)];
+                compGames = compGames.concat(userGames);
+                for (friend of friends) {
+                    let friendGames = await api.getGames(friend)
+                    // Need to make friendGames a set because they can have multiple of the same game.
+                    let set = [...new Set(friendGames)];
+                    compGames = compGames.concat(set)
+                }
+                for (let i = 0; i < compGames.length; i++) {
+                    let num = compGames[i];
+                    counts[num] = counts[num] ? counts[num] + 1 : 1;
+                }
+                for (count in counts) {
+                    if (counts[count] === totalUsers) {
+                        sameGames.push(count)
+                    }
+                }
+                res.send(sameGames)
+            }
+        } else {
+            res.redirect('/auth/login')
+        }
+    });
+
+    return app
+};
