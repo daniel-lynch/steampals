92d40dc6990ab93137cd3de7098a8d459c2c3dc1
Anthony Loukinas <anthony.loukinas@gmail.com>
2019-09-03 22:07:37 -0400

    Core Architecture Update

---
+++ routes/auth.server.routes.js
@@ -0,0 +1,44 @@
+/**
+ * Auth Server Routes
+ * @author Anthony Loukinas <anthony.loukinas@redhat.com>
+ */
+
+/**
+ * Module dependencies
+ */
+const passport      = require('passport'),
+  passportService   = require('../lib/passport'),
+  Auth              = require('../controllers/auth.server.controller');
+
+/**
+ * Use Passport Authentication Strategies
+ * @type {AuthenticateRet}
+ */
+const requireLogin = passport.authenticate(
+  ['steam'], {
+    successRedirect: '/',
+    failureRedirect: '/auth/login',
+    failureFlash: false
+  }
+)
+
+
+/**
+ * Route handling logic
+ * @param app - Express Application
+ */
+module.exports = function(app) {
+
+  app.route('/auth/openid')
+    .post(requireLogin);
+
+  app.route('/auth/openid/return')
+    .get(requireLogin, Auth.getReturn);
+
+  app.route('/auth/logout')
+    .post(Auth.postLogout);
+
+  app.route('/auth/login')
+    .get(Auth.getLogin);
+    
+}
