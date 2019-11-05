479fd9f2c7ffdf96c0b50850bfdcd403736f002e
dj@loukihost.com <daniel.lynch2016@gmail.com>
2019-11-04 19:49:24 -0500

    lots of changes searchbar now filters text more componets and subcomponets

--- src/componets/ProfileCard.jsx
+++ src/componets/ProfileCard.jsx
@@ -9,17 +9,19 @@ import '../index.css';
 class ProfileCard extends Component {
     render(){
         return (
-            <Card style={{ width: '22rem' }}>
-                <Card.Body className="friendsCard">
-                    <Row>
-                        <Col xs="auto"><img alt="" src={this.props.pic} /></Col>
-                        <Col xs="auto">
-                            <p className={this.props.type + "Name"}>{this.props.name}</p>
-                            <p className={this.props.type}>{this.props.status}</p>
-                        </Col>
-                    </Row>
-                </Card.Body>
-            </Card>
+            <Col md={4} style={{marginTop: '20px'}}>
+                <Card>
+                    <Card.Body className="friendsCard">
+                        <Row>
+                            <Col xs="auto"><img alt="" src={this.props.pic} /></Col>
+                            <Col xs="auto">
+                                <p className={this.props.type + "Name"}>{this.props.name}</p>
+                                <p className={this.props.type}>{this.props.status}</p>
+                            </Col>
+                        </Row>
+                    </Card.Body>
+                </Card>
+            </Col>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -10,45 +10,125 @@ import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import './index.css'
 
+class Board extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            filterText: '',
+            friends : [
+                {
+                    id: 1, 
+                    pic: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/48/48fdc52e37e59e0c8352d069aef3ac24f6586b0f_medium.jpg",
+                    name: "Toxic Laughter",
+                    status: "Online",
+                    type: "online"
+                },
+                {
+                    id: 2,
+                    pic: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/0d/0db8ba11140bc3123cff7176121949aec93d5617_medium.jpg",
+                    name: "antonio",
+                    status: "Call of Duty",
+                    type: "inGame",
+                },
+                {
+                    id:3,
+                    pic: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/9d/9db9a42b946084eb4446a94ec0342f163dff6f0e_medium.jpg",
+                    name: "imfearless",
+                    status: "Away",
+                    type: "away"
+                },
+                {
+                    id: 4, 
+                    pic: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/48/48fdc52e37e59e0c8352d069aef3ac24f6586b0f_medium.jpg",
+                    name: "Toxic Laughter",
+                    status: "Online",
+                    type: "online"
+                },
+                {
+                    id: 5,
+                    pic: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/0d/0db8ba11140bc3123cff7176121949aec93d5617_medium.jpg",
+                    name: "antonio",
+                    status: "Call of Duty",
+                    type: "inGame",
+                },
+                {
+                    id: 6,
+                    pic: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/9d/9db9a42b946084eb4446a94ec0342f163dff6f0e_medium.jpg",
+                    name: "imfearless",
+                    status: "Away",
+                    type: "away"
+                },
+            ]
+        }
+
+        this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+
+      }
+
+    handleFilterTextChange(filterText) {
+        this.setState({
+            filterText: filterText
+        });
+    }
+
+    render(){
+        const filterText = this.state.filterText.toLowerCase();
+        const rows = [];
+
+        this.state.friends.forEach((friend) => {
+            if (friend.name.toLowerCase().indexOf(filterText) === -1){
+                console.log(filterText)
+                return;
+            }
+            rows.push(<ProfileCard pic={friend.pic} name={friend.name} status={friend.status} type={friend.type} key={friend.id} />)
+        })
+
+        return (
+            <Row>
+                <Col md={4}>
+                    <h2 className="friendsNav">Select Your friends</h2>
+                </Col>
+                <Col md={4}>
+                    <SearchBar filterText={this.state.filterText} onFilterTextChange={this.handleFilterTextChange} />
+                </Col>
+                <Col md={4}>
+                    <Button variant="success" className="w-100">Compare with Friends!</Button>
+                </Col>
+                {rows}
+            </Row>
+        )
+    }
+}
+
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+      }
+
+      handleFilterTextChange(e) {
+        this.props.onFilterTextChange(e.target.value);
+      }
+
+      render() {
+        return (
+        <Form.Control
+            type="text"
+            placeholder="Search..."
+            value={this.props.filterText}
+            onChange={this.handleFilterTextChange} />
+        );
+    }
+}
+
 
 // TODO make friends nav component
 
 const App = () => (
     <Container>
         <Nav />
-        <Row>
-            <Col md={4}>
-                <h2 className="friendsNav">Select Your friends</h2>
-            </Col>
-            <Col md={4}>
-                <Form.Control type="email" placeholder="Search..." />
-            </Col>
-            <Col md={4}>
-                <div>
-                    <Button variant="success" className="d-block w-100 ml-auto">Compare with Friends!</Button>
-                </div>
-            </Col>
-        </Row>
-        <Row>
-            <Col md={4} style={{marginTop: '20px'}}>
-                <ProfileCard pic="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/48/48fdc52e37e59e0c8352d069aef3ac24f6586b0f_medium.jpg" name="Toxic Laughter" status="Online" type="online" />
-            </Col>
-            <Col md={4} style={{marginTop: '20px'}}>
-                <ProfileCard pic="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/0d/0db8ba11140bc3123cff7176121949aec93d5617_medium.jpg" name="antonio" status="Call of Duty" type="inGame" />
-            </Col>
-            <Col md={4} style={{marginTop: '20px'}}>
-                <ProfileCard pic="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/9d/9db9a42b946084eb4446a94ec0342f163dff6f0e_medium.jpg" name="imfearless" status="Away" type="away" />
-            </Col>
-            <Col md={4} style={{marginTop: '20px'}}>
-                <ProfileCard pic="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/48/48fdc52e37e59e0c8352d069aef3ac24f6586b0f_medium.jpg" name="Toxic Laughter" status="Online" type="online" />
-            </Col>
-            <Col md={4 } style={{marginTop: '20px'}}>
-                <ProfileCard pic="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/0d/0db8ba11140bc3123cff7176121949aec93d5617_medium.jpg" name="antonio" status="Call of Duty" type="inGame" />
-            </Col>
-            <Col md={4} style={{marginTop: '20px'}}>
-                <ProfileCard pic="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/9d/9db9a42b946084eb4446a94ec0342f163dff6f0e_medium.jpg" name="imfearless" status="Away" type="away" />
-            </Col>
-        </Row>
+        <Board />
     </Container>
   );
 
